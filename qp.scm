aad3edd48c4359e461c3ac79be72c71a192e129e
Eddie Monroe <eddiemonroe@gmail.com>
2019-03-04 15:28:25 -0800

    refactoring and additions

--- qp.scm
+++ qp.scm
@@ -10,25 +10,66 @@
 	(opencog nlp sureal)
 )
 
-(define (parse-get-verb-obj parse)
-	(define bl
-		(BindLink
-			(And
-				(Evaluation
-					(DefinedLinguisticRelationship "_obj")
-					(List (Variable "$word-inst-pred") (Variable "$word-inst-obj")))
-				(WordInstanceLink (Variable "$word-inst-pred") parse)
-				(WordInstanceLink (Variable "$word-inst-obj") parse)
-			)
-			(List (Variable "$word-inst-pred") (Variable "$word-inst-obj"))
-		)
- 	)
- (gar (cog-execute! bl))
+; -----------------------------------------------------------------------
+; Todo: handle muliple relations of the same type
+; Get the relex relations of a given relationship type
+(define (parse-get-relex-relation-type parse rel-type)
+  (define gl
+    (GetLink
+      ; Defining type here to avoid returning VariableNodes
+      (VariableList
+        (TypedVariableLink
+          (VariableNode "$word-inst-head")
+          (TypeNode "WordInstanceNode"))
+        (TypedVariableLink
+          (VariableNode "$word-inst-dependent")
+          (TypeNode "WordInstanceNode")))
+      (And
+        (Evaluation
+          (DefinedLinguisticRelationshipNode rel-type)
+          (List (Variable "$word-inst-head") (Variable "$word-inst-dependent")))
+        (WordInstanceLink (Variable "$word-inst-head") parse)
+        (WordInstanceLink (Variable "$word-inst-dependent") parse)
+      )
+    )
+  )
+  (gar (cog-execute! gl))
 )
 
-(define (parse-get-obj parse)
-  (gdr (parse-get-verb-obj parse)))
+(define (parse-get-obj-relation parse)
+  (parse-get-relex-relation-type parse "_obj"))
 
+
+(define (parse-get-subj-relation parse)
+  (parse-get-relex-relation-type parse "_subj"))
+
+
+; Replaced by above (parse-get-obj-relation)
+#!
+(define (parse-get-verb-obj parse)
+  (define bl
+    (BindLink
+      (And
+        (Evaluation
+          (DefinedLinguisticRelationshipNode "_obj")
+          (List (Variable "$word-inst-verb") (Variable "$word-inst-obj")))
+        (WordInstanceLink (Variable "$word-inst-verb") parse)
+        (WordInstanceLink (Variable "$word-inst-obj") parse)
+      )
+      (List (Variable "$word-inst-pred") (Variable "$word-inst-obj"))
+    )
+  )
+ (gar (cog-execute! bl))
+)
+!#
+
+(define (parse-get-obj parse)
+	(let ((verb-obj (parse-get-obj-relation parse)))
+	  (if (null? verb-obj)
+	    '()
+	    (gdr verb-obj))))
+
+; Todo: Handle multiple verbs
 (define (parse-get-verb parse)
   (gar
     (cog-execute!
@@ -41,13 +82,14 @@
         (VariableNode "$word-instance-verb")))))
 
 
-
+; -----------------------------------------------------------------------
 ; Utilities
 
 ; Get the WordNode for the given WordInstance
-(define (instance-get-word instance)
+(define (word-inst-get-word instance)
   (car (cog-chase-link 'ReferenceLink 'WordNode instance)))
 
+; Use word-inst-get-lemma in relex-utils.scm instead
 ; Get the lemma for a given WordInstance
 (define (instance-get-lemma instance)
   (car (cog-chase-link 'LemmaLink 'WordNode instance)))
@@ -57,45 +99,63 @@
 (define gsent)
 (define gparse)
 (define grelex)
+(define grelations)
 (define gr2l)
-(define gverb-obj)
+(define gverb)
+(define gsubj-rel)
+(define gobj-rel)
 (define gobj)
+(define gpobj)
 
+; -----------------------------------------------------------------------
 (define (qp text)
 
   ; Todo: Handle multiple sentences? Not sure if parser returns multiples.
   (define sent (car (nlp-parse text)))
 
   ; For now we are just using the first parse
-  ; Todo: Use multipe parses, if parser returns multiple
+  ; Todo: Use multiple parses, if parser returns multiple
   (define parse (car (sentence-get-parses sent)))
 
   (define relex (parse-get-relex-outputs parse))
   ; relex
 
+  (define relations(parse-get-relex-relations parse))
+
   (define r2l (sent-get-r2l-outputs sent))
   ;r2l
- 
-  (define verb-obj (parse-get-verb-obj parse))
+
+  (define verb (parse-get-verb parse))
+
+  (define subj-rel (parse-get-subj-relation parse))
+
+  (define obj-rel (parse-get-obj-relation parse))
 
   (define obj (parse-get-obj parse))
-  ; parse-obj
+
+  (define pobj-rel (parse-get-relex-relation-type parse "_pobj"))
 
  ; Temp for debugging
  (set! gsent sent)
  (set! gparse parse)
  (set! grelex relex)
+ (set! grelations relations)
  (set! gr2l r2l)
- (set! gverb-obj verb-obj)
+ (set! gverb verb)
+ (set! gsubj-rel subj-rel)
+ (set! gobj-rel obj-rel)
  (set! gobj obj)
+ (set! gpobj-rel pobj-rel)
  
- parse-obj
+ subj-rel
 
 )
 
 
-Todo: How to deal with sentences with multiple subj-verb-obj triplets. E.g., "Do
-you want to eat ice cream."
 
+
+; Todo: How to deal with sentences with multiple subj-verb-obj triplets. E.g.,
+; "Do you want to eat ice cream."
+; Todo: Add nn DefinedLigusticConcept, as in for "ice cream"
 
 
