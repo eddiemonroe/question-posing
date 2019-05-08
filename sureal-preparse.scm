a9bc5f17f91eea197082d2599811c237a03330e1
Eddie Monroe <eddiemonroe@gmail.com>
2019-05-08 11:11:10 -0700

    add preparsed sureal sentences file

---
+++ sureal-preparse.scm
@@ -0,0 +1,7 @@
+(nlp-parse "were you interested?")
+(nlp-parse "I am entertained")
+(nlp-parse "I wanted to relax")
+
+(nlp-parse "Were you interested?")
+(nlp-parse "You are entertained")
+(nlp-parse "You wanted to relax")
