a76ddfd6dca5a0d855778f29b0308763efcd1c68
Eddie Monroe <eddiemonroe@gmail.com>
2019-05-08 10:58:39 -0700

    implement workaround for sureal bug for Evals with no List wrapping args

--- qp-nlp.scm
+++ qp-nlp.scm
@@ -19,6 +19,9 @@
 ;; Load rules particular to question processing
 (load "rules.scm")
 
+;Pre-parsed sentences for Sureal
+(load "sureal-preparse.scm")
+
 ; --- Input Utterance --- ;
 ;; Parse the input utterance
 ;; Todo: Handle multiple sentence interpretations
@@ -63,7 +66,7 @@
 ;; Temp hacky way to convert "I" to "you"
 (set! utter-abstract (cog-execute! i-to-you-rule))
 
-;; Temp hack to set confidence values above 0 for instantiation rules
+;; Set confidence values above 0 for instantiation rules
 (for-each
   (lambda (eval-link) (cog-set-tv! eval-link (stv 1 1)))
   (cog-outgoing-set utter-abstract))
@@ -98,18 +101,57 @@
 ;; Results in BL's for every ImplicationScope in the KB
 (define meta-bind-results
   (meta-bind conditional-full-instantiation-implication-scope-meta-rule))
-(format #t "meta-bind-results:\n~a" meta-bind-results)
+(format #t "meta-bind-results:\n~a\n" meta-bind-results)
 
 ; Testing
 ;; This creates BindLink rules for each ImplicationScopeLink
 ;; Do we instead just want to grab matching Implications and create BL's on the fly?
-(define rules (cog-execute!
-  conditional-full-instantiation-implication-scope-meta-rule))
+;;(define rules (cog-execute!
+;;conditional-full-instantiation-implication-scope-meta-rule))
 
 ;; -------------------------------------
-;; Surreal question generation
+;; Sureal question generation
 
-  ;For SuReal
-  (nlp-parse "were you interested?")
-  (nlp-parse "I am entertained")
-  (nlp-parse "I wanted to relax")
+;; Temp hack to workaround sureal bug for Eval's without List for args
+(define (add-list-to-eval eval)
+  (if (not (equal? (cog-type (gar eval)) 'ListLink))
+  ;; Assumption: if no List for arg, then we know it is a single argument,
+  ;; though this will be changing, i.e., mult Eval args without list
+  (let ([pred (gar eval)]
+    [arg (gdr eval)])
+    (Evaluation pred (List arg)))
+    eval))
+
+(set! meta-bind-results
+  (Set (map add-list-to-eval (cog-outgoing-set meta-bind-results))))
+
+; Generate the sentence
+(define questions
+  (append-map
+    (lambda (result-logic)
+      (sureal
+        (Set
+          result-logic
+          (Inheritance (InterpretationNode "blah") (DefinedLinguisticConcept "TruthQuerySpeechAct")))))
+    (cog-outgoing-set meta-bind-results)))
+
+(format #t "generated questions:\n~a\n" questions)
+
+;; ---------------------------------------
+;; Testing
+(define (sureal-for-logic logic)
+  (sureal
+    (Set
+      logic
+;      (Inheritance (InterpretationNode "blah") (DefinedLinguisticConcept "TruthQuerySpeechAct"))
+
+      (Inheritance (InterpretationNode "blah")
+                   (DefinedLinguisticConcept "TruthQuerySpeechAct"))
+    )))
+
+(define r1 (gar meta-bind-results))
+(define rlist
+  (Evaluation
+    (gar r1)
+    (List
+      (gdr r1))))
